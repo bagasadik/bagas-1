e7d85ca0806536df5d33d3fcd23912c30f698500
bagasadiik <34002286+bagasadiik@users.noreply.github.com>
2017-11-26 19:58:30 +0700

    Update fft.js

--- fft.js
+++ fft.js
@@ -12,11 +12,11 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'bagasadik',
+    password: 'Jakarta1243'
 }
 
-const TargetUsername = '';
+const TargetUsername = 'andikahonda18';
 
 const Login = async function(User){
 
